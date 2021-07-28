94ad4a088ea9c461bc408d207a9443822519238b
crono628 <84817352+crono628@users.noreply.github.com>
2021-07-28 17:21:16 +0000

    basic operators

--- calculator.js
+++ calculator.js
@@ -1,5 +1,28 @@
-function add(...args) {
-  return arg.reduce(acc, cur){
-    return acc + cur
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+let operatorSelect = function (operator, a, b) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      if (b === 0) {
+        return 'ERROR';
+      } else {
+        return divide(a, b);
+      }
+    default:
+      return null;
   }
 }
+
+
+
+console.log(operatorSelect('+', 15,25))
+console.log('test')
