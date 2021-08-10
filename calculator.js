477c227b30ba325a28b9b13c95799c676f5e12ff
crono628 <m.desantis628@gmail.com>
2021-08-10 06:30:42 -0400

    still working

--- calculator.js
+++ calculator.js
@@ -1,10 +1,14 @@
-const btn = document.querySelectorAll('.btn, .operatorBtn');
+const btn = document.querySelectorAll('.btn');
 const output = document.querySelector('#output');
 const clearBtn = document.querySelector('#clearBtn');
 const equalsBtn = document.querySelectorAll('#equalsBtn');
 const operatorBtn = document.querySelectorAll('.operatorBtn');
 
-let ready = true;
+let ready = false;
+let display = [];
+let hold = [];
+let calculation = [];
+let solution = [];
 
 function notReady() {
   ready = false;
@@ -14,67 +18,75 @@ function isReady() {
   ready = true;
 }
 
-
-let temp = [];
-let display = [];
-let calculation = [];
-let solution = [];
-
+function allClear() {
+  hold = [];
+  display = [];
+  calculation = [];
+  solution = [];
+  notReady();
+}
 
 btn.forEach((button) => {
   button.addEventListener('click', () => {
-    output.textContent += button.textContent;
-    notReady()
-    console.log(ready)
+    display.push(button.textContent);
+    hold.push(button.textContent);
+    calculation.unshift(hold.join(''))
+    
+    output.textContent = display.join('')
+    isReady();
+    // console.log(calculation);
+    // calculation.shift();
   });
 });
 
 operatorBtn.forEach((button) => {
   button.addEventListener('click', () => {
-    operatorBtn.textContent = temp.push(), calculation.push();
-    isReady()
-    // logNum()
-    console.log(ready);
+    hold = [];
+    if (ready == false) {
+      return;
+    } else {
+      display.push(button.textContent);
+      // hold.push(button.textContent);
+      calculation.push(button.textContent);
+      output.textContent = display.join('');
+      notReady();
+      console.log(calculation);
+
+    }
   })
 })
 
 clearBtn.addEventListener('click', () => {
   output.textContent = "";
-  reset();
-  console.log(ready);
+  allClear()
 });
 
 equalsBtn.forEach((button) => {
   button.addEventListener('click', () => {
-    operatorSelect(calculatorObj.operatorObj, calculatorObj.operandA, calculatorObj.operandB);
+
+
   })
 });
-
-
 
 let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
 
-let operatorSelect = function (operator, a, b) {
+let doMath = function (operator, a, b) {
   switch (operator) {
-    case '+':
+    case ' + ':
       return add(a, b);
-      break;
-    case '-':
+    case ' - ':
       return subtract(a, b);
-       break;
-    case '*':
+    case ' * ':
       return multiply(a, b);
-       break;
-    case '/':
+    case ' / ':
       if (b === 0) {
         return 'ERROR';
-         break;
       } else {
         return divide(a, b);
-         break;
       }
   }
 }
+
