84f01759afb7ee87a70379e4eb709752658a1362
crono628 <84817352+crono628@users.noreply.github.com>
2021-09-28 18:14:29 +0000

    started js

--- calculator.js
+++ calculator.js
@@ -3,11 +3,34 @@ const bottomDisplay = document.querySelector('.bottom-display')
 const topDisplay = document.querySelector('.top-display')
 
 buttons.forEach(button => {
-  button.addEventListener('click',() => {
-    bottomDisplay.textContent += button.dataset.btn
-  })
+  button.addEventListener('click', () => {
+    let buttonData = button.dataset.btn
+    bottomDisplay.textContent += buttonData
+    if (buttonData === 'c') {
+      clear()
+    }
+    // if (buttonData === '='){
+    //   calculate()
+    // }
+  }) 
 })
 
-function operation (){
-  
+function clear() {
+  bottomDisplay.textContent = ''
+  topDisplay.textContent = ''
+}
+
+function calculate(operator, a, b) {
+  if (a && b) {
+    switch (operator) {
+      case '+': parseFloat(a + b)
+        break;
+      case '-': parseFloat(a - b)
+        break;
+      case '*': parseFloat(a * b)
+        break;
+      case '/': parseFloat(a / b)
+        break;
+    }
+  }
 }
